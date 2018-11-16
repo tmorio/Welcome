c4ed842a8133e8d4fa196d805064781182be21b9
tmorio <morikapusan@eleken.jp>
2018-11-16 23:25:25 +0900

    First Commit

---
+++ helloworld.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class ViewController: UIViewController {
+	override func viewDidLoad() {
+	println("Hello World")
+	super.viewDidLoad()
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+	}
+}
